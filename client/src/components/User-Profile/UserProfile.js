f83852bd731465126e3bea4c2605dd51b9dc1d25
jasn6 <110363877+jasn6@users.noreply.github.com>
2023-08-10 14:44:51 -0400

    Adding more design to user profile

--- client/src/components/User-Profile/UserProfile.js
+++ client/src/components/User-Profile/UserProfile.js
@@ -6,6 +6,21 @@ const UserProfile = ({user, email, pfp, setPfp, isOpen, onClose}) => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [error, setError] = useState('');
 
+  const truncateFileName = (name) => {
+    const maxLength = 15; // Adjust this value according to your preference
+
+    if (name.length <= maxLength) return name;
+
+    const extension = name.split('.').pop();
+    const mainName = name.substr(0, name.length - extension.length - 1); // name without the extension
+
+    // Calculate the number of characters we can take from the mainName before and after the '...'
+    const sliceLength = (maxLength - 4) / 2; // 4 accounts for the length of '...' and the dot of the extension
+
+    return `${mainName.substr(0, sliceLength)}...${mainName.substr(-sliceLength)}.${extension}`;
+  }
+
+
   const handleFileChange = (e) => {
     setSelectedFile(e.target.files[0]);
     setError(''); // Reset error
@@ -60,28 +75,35 @@ const UserProfile = ({user, email, pfp, setPfp, isOpen, onClose}) => {
     }
   };
   return (
-         <div className={`profileModal ${isOpen ? 'open' : 'closed'}`}>
-           <div className="profileModal-content">
-              <span className="closeProfile" onClick={onClose}>&times;</span>
-              <h2 className="UserProfileHeader">User Profile</h2>
-              
-              <div className="pfpConatiner">
-                <img src={`http://localhost:3001${pfp}`} alt="User's profile" />
-                <form onSubmit={e => e.preventDefault()}>
-                  <input type="file" onChange={handleFileChange} />
-                  <button type="button" onClick={handleUpload}>Upload</button>
-                </form>
-                {error && <p style={{ color: 'red' }}>{error}</p>}
-              </div>
-              
-              <div className="profile-details">
-                  <p><strong>Username:</strong> {user}</p>
-                  <p><strong>Email:</strong> {email}</p>
-              </div>
-              
-              <button className="logout-button" onClick={logoutUser}>Logout</button>
+    <div className={`profileModal ${isOpen ? 'open' : 'closed'}`}>
+      <div className="profileModal-content">
+        <span className="closeProfile" onClick={onClose}>&times;</span>
+        <h2 className="UserProfileHeader">My Profile</h2>
+        
+        <div className="pfpContainer">
+          <img src={`http://localhost:3001${pfp}`} alt="User's profile" />
+          <form className="uploadForm" onSubmit={e => e.preventDefault()}>
+            <div>
+               <label className="customFileInput">
+                Choose File
+                <input type="file" onChange={handleFileChange} />
+               </label>
+              <span className="fileNameDisplay">{selectedFile && truncateFileName(selectedFile.name)}</span>
             </div>
+            <button className="uploadButton" type="button" onClick={handleUpload}>Upload</button>
+         </form>
+          {error && <p className="errorMsg">{error}</p>}
         </div>
+        
+        <div className="profile-details">
+            <p><strong>Username:</strong> {user}</p>
+            <p><strong>Email:</strong> {email}</p>
+        </div>
+        
+        <button className="logout-button" onClick={logoutUser}>Logout</button>
+      </div>
+    </div>
+
   );
 }
 
